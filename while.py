8a148c8145fc59024e76fcc9cd15f5fa8bb2704b
ANVARJONOV NURMUHAMMAD <shahlohonteshaboyov@gmail.com>
2025-02-02 23:14:49 +0500

    Create while.py

---
+++ while.py
@@ -0,0 +1,36 @@
+# 1- m
+# print(" Sonlarni kvadiratini chiqarib beradigan dastur ")
+# yegindi = " "
+# while yegindi != "tohta" :
+#     yegindi =input(" Istol gan son kiriting yoki (dasturni tohtatish uchun tohta deb yozing) : ")
+#     if yegindi != "tohta":
+#         print(float(yegindi)**2)
+# print(" Dastur tugadi ")
+# 2 - m
+# print("#Istalgan soni kivadiratini topadigan dastur ")
+# yegindi = " "
+# ishora = True
+# while ishora:
+#     yegindi= input("Istalgan son kiriting (dasturni tohtatish uchun tohta deb yozing )")
+#     if yegindi =="tohta":
+#         ishora = False
+#     else:
+#         print(float(yegindi)**2)
+# print("Dastur tugadi")
+# 3 - m
+# print("#Istlagan soni kivadiratini chiqarib beradigan dastur ")
+# yegindi = " "
+# while True:
+#     yegindi = input("Istalgan son kiriting (datsturni tohtatish uchun tohta deb yozing )")
+#     if yegindi == "tohta":
+#         break
+#     else:
+#         print(float(yegindi)**2)
+# print("Dastur tohtadi ")
+# 4- m
+# sonlare =(range(1,11))
+# for son in sonlare:
+#     if son ==4:
+#         break
+#     else:
+#         print(f"{son } bu soni kivadirati {son **2 }")
